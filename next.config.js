f917130078d720c45756028dd84458775910d768
Rajat Chaudhary <chaudharyrajat80@gmail.com>
2022-10-16 19:51:36 +0530

    Serve tic-tac-toe and wordle

--- next.config.js
+++ next.config.js
@@ -1,3 +1,9 @@
 module.exports = {
     distDir: "build",
+    rewrites: async () => [
+        {
+          source: "/public/wordle/index.html",
+          destination: "/pages/api/wordle.js",
+        },
+      ],
 };
